1ca0d42f8d30d83dde83d3011b9b91ba8033b907
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-17 17:22:37 +0530

    Simplify functions

--- lib/cache.ex
+++ lib/cache.ex
@@ -8,52 +8,32 @@ defmodule Cache do
 
   def get(key) do
     file = get_file(key)
-    {:ok, contents} = read(file, File.regular?(file))
-    result = JSX.decode(contents)
-    result
-  end
-
-  def read(file, true) do
-    result = File.read(file)
-    result
-  end
-
-  def read(_file, false) do
-    result = {:ok, "null"}
+    result =
+      case File.regular?(file) do
+        true ->
+          contents = File.read!(file)
+          contents = JSX.decode!(contents)
+          {:ok, contents}
+        false ->
+          {:error, nil}
+      end
     result
   end
 
   def set(key, value) do
     file = get_file(key)
-    result = JSX.encode(value, [{:space, 1}, {:indent, 4}])
-    result = write(file, result)
-    result
-  end
-
-  def write(file, {:ok, contents}) do
-    result = File.write(file, contents, [:utf8, :write])
-    result
-  end
-
-  def write(_file, {:error, reason}) do
-    result = {:error, reason}
-    result
+    contents = JSX.encode!(value, [{:space, 1}, {:indent, 4}])
+    :ok = File.write!(file, contents, [:utf8, :write])
+    :ok
   end
 
   def get_file(arguments) do
     file = Path.join([Application.get_env(:cache, :path)] ++ arguments)
     directory = Path.dirname(file)
-    file = get_file(directory, file, File.dir?(directory))
-    file
-  end
-
-  def get_file(_directory, file, :true) do
-    file
-  end
-
-  def get_file(directory, file, :false) do
-    :ok = File.mkdir_p(directory)
-    :ok = File.chmod(directory, 00755)
+    unless File.dir?(directory) do
+      :ok = File.mkdir_p!(directory)
+      :ok = File.chmod!(directory, 00755)
+    end
     file
   end
 end
