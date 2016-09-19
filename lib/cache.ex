4ea57e31cf50eec7a13171d6b0c4bcc6f495231b
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-19 14:26:04 +0530

    mix credo --strict && mix dogma

--- lib/cache.ex
+++ lib/cache.ex
@@ -8,16 +8,14 @@ defmodule Cache do
 
   def get(key) do
     file = get_file(key)
-    result =
-      case File.regular?(file) do
-        true ->
-          contents = File.read!(file)
-          contents = JSX.decode!(contents)
-          {:ok, contents}
-        false ->
-          {:error, nil}
-      end
-    result
+    case File.regular?(file) do
+      true ->
+        contents = File.read!(file)
+        contents = JSX.decode!(contents)
+        {:ok, contents}
+      false ->
+        {:error, nil}
+    end
   end
 
   def set(key, value) do
@@ -26,8 +24,8 @@ defmodule Cache do
     File.write!(file, contents, [:write])
   end
 
-  def get_file(arguments) do
-    file = Path.join([Application.get_env(:cache, :path)] ++ arguments)
+  def get_file(key) do
+    file = Path.join([Application.get_env(:cache, :path)] ++ key)
     directory = Path.dirname(file)
     unless File.dir?(directory) do
       :ok = File.mkdir_p!(directory)
